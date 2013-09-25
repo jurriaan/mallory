c04fe5aef584561512d760c6b7980380795a4001
Marcin Sawicki <odcinek@gmail.com>
2013-09-25 02:44:30 +0200

    Initial commit

--- keys/keygen.sh
+++ keys/keygen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Self signed dummy cert"
 set -e
-openssl genrsa -out server.key 1024
-openssl req -subj '/C=US/ST=CA/L=SF/CN=*.com' -new -key server.key -out server.csr
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+openssl genrsa -out ./keys/server.key 1024
+openssl req -subj '/C=US/ST=CA/L=SF/CN=*.com' -new -key ./keys/server.key -out ./keys/server.csr
+openssl x509 -req -days 365 -in ./keys/server.csr -signkey ./keys/server.key -out ./keys/server.crt
