09e3698cae42e9bd9b6a5eae02265cd99309da41
Marcin Sawicki <odcinek@gmail.com>
2013-09-27 22:36:22 +0200

    Proper way of handling requests with tests

--- spec/mallory/response_spec.rb
+++ spec/mallory/response_spec.rb
@@ -1,41 +1,29 @@
 require 'spec_helper'
-require 'thin'
 require 'em-http-request'
 require 'mallory/response'
 require 'responder'
 
 describe EventMachine::Mallory::Response do
 
-  before(:all) do
-    Thread.new { EM.run  }
-    trap(:INT) { EM.stop }
-    trap(:TERM){ EM.stop }
-    web = Thin::Server.new(Responder, '127.0.0.1', 6701)
-    web.silent = true
-    web.start
-    sleep 5
-    p web.running?
-  end
-
-  after(:all) do
-    EM.stop
-  end
-
   it "should 1" do
-    options = {}
-    http = EventMachine::HttpRequest.new("http://127.0.0.1:6701/200", options).get
-    http.callback {
-      p "SUCCESS"
-      p http.response_header.status
-      p http.response_header
-      p http.response
-    }
-    http.errback {
-      p "ERRBACK"
-      p http.error
-    }
+    EM.run do
+      http = EventMachine::HttpRequest.new("http://127.0.0.1:6701/200", {}).get
+      http.callback do
+        p "SUCCESS"
+        EM.stop
+      end
+      http.errback { EM.stop; raise }
+    end
   end
 
   it "should 2" do
+    EM.run do
+      http = EventMachine::HttpRequest.new("http://127.0.0.1:6701/200", {}).get
+      http.callback do
+        p "SUCCESS"
+        EM.stop
+      end
+      http.errback { EM.stop; raise }
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,22 @@
 require 'rspec'
-require 'thin'
+require 'webrick'
 require 'em-http-request'
 require 'responder'
 
 RSpec.configure do |config|
   config.order = 'random'
 
-#  config.before(:all) do
-#    Thread.new { EM.run  }
-#    trap(:INT) { EM.stop }
-#    trap(:TERM){ EM.stop }
-#    web = Thin::Server.new(Responder, '127.0.0.1', 6701)
-#    web.silent = true
-#    web.start
-#    p web.running?
-#    sleep 5
-#  end
-
-#  config.after(:all) do
-#    EM.stop
-#  end
+  config.before(:suite) do
+    trap(:INT) { EM.stop }
+    trap(:TERM){ EM.stop }
+    Thread.new do
+      Rack::Handler::WEBrick.run(
+        Responder.new,
+        :Port => 6701,
+        :AccessLog => [],
+        :Logger => WEBrick::Log::new("/dev/null", 7))
+    end
+    sleep 3
+  end
 
 end
