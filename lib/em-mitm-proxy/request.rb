f577eaa512d3e3e87be5463f3dced12b42abf2ad
Marcin Sawicki <odcinek@gmail.com>
2013-09-27 13:47:44 +0200

    Specs for Request class
    WEBrick::HTTPStatus::BadRequest is kinda weird and WEBrick seems
    to have vague rules for throwing it. Might be worth investigating.

--- lib/em-mitm-proxy/request.rb
+++ lib/em-mitm-proxy/request.rb
@@ -5,21 +5,21 @@ module Mitm
 
     attr_accessor :proto
     def initialize data
-      @proto = "http"
-      begin
-        host = data.match(/Host:\s(.*)\r/)[1]
-        proto = data.match(/[A-Z]{3,8}\s(http[\d+])/)[1] rescue "http"
-        data.sub!("http://#{host}","")
-        @request = WEBrick::HTTPRequest.new(WEBrick::Config::HTTP)
-        @request.parse(StringIO.new(data))
-      rescue => e
-        @logger.debug e.message
-        raise
-      end
+      line = data.match(/([A-Z]{3,8})\s(?:(http[\w+]*):\/\/(\w*):*(\d{2,5})*)*(\/{0,1}.*)\sHTTP/)
+      method = line[1]
+      @protocol = line[2]
+      port = line[4] 
+      path = line[5]
+      host = line[3] || data.match(/Host:\s(.*)\n/)[1]
+      data.sub!("http://#{host}","")
+      @request = WEBrick::HTTPRequest.new(WEBrick::Config::HTTP)
+      @request.parse(StringIO.new(data))
+    rescue WEBrick::HTTPStatus::BadRequest
+      raise
     end
 
     def uri
-      "#{@proto}://#{@request['host']}#{@request.path}"
+      "#{@protocol}://#{@request['host']}#{@request.path}"
     end
 
     def method
@@ -34,6 +34,8 @@ module Mitm
 
     def body
       @request.body
+    rescue WEBrick::HTTPStatus::LengthRequired
+      nil
     end
 
   end
---
+++ spec/em-mitm-proxy/request_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'em-mitm-proxy/request'
+
+describe "Mitm::Request" do
+
+  methods = ['GET', 'POST', 'HEAD', 'PUT', 'CONNECT', 'DELETE']
+
+  good_requests = [
+    {:path => "/", :host => "localhost"},
+    {:path => "/index.html", :host => "localhost"},
+    {:path => "/index.html?test", :host => "localhost:80"},
+    {:path => "http://localhost/index.html", :host => "localhost"},
+    {:path => "http://localhost/", :host => "localhost"},
+    {:path => "https://localhost/", :host => "localhost:443"}
+  ]
+
+  bad_requests = [
+    {:path => "http://loca lhost :6700", :host => "localhost:6700"}
+    #{:path => "http://localhost:6700/", :host => "localhost:6700"},
+    #{:path => "http://localhost:6700/index.html", :host => "localhost"}
+  ]
+
+  good_requests.each do |request|
+    it "should accept #{request[:path]}" do
+      methods.each do |method|
+        body =<<-HTTP.gsub(/^ +/, '')
+          #{method} #{request[:path]} HTTP/1.1
+          Host: #{request[:host]}
+        HTTP
+        rq = Mitm::Request.new(body)
+        rq.method.should eq(method.downcase)
+        rq.body.should be(nil)
+      end
+    end
+  end
+
+  bad_requests.each do |request|
+    it "should raise on #{request[:path]}" do
+      methods.each do |method|
+        body =<<-HTTP.gsub(/^ +/, '')
+          #{method} #{request[:path]} HTTP/1.1
+          Host: #{request[:host]}
+        HTTP
+        expect { Mitm::Request.new(body) }.to raise_error
+      end
+    end
+  end
+
+end
